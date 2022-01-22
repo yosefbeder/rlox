632928f2c88a8527c668013a745f65b8eac3183f
yosefbeder <dryosefbeder@gmail.com>
2022-01-22 09:47:04 +0200

    feat(errors): implement std::error::Error trait

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,13 @@ pub mod parser;
 pub mod scanner;
 use std::fmt;
 
+#[derive(Debug)]
 pub enum Error {
     Syntax { message: String, line: usize },
     Runtime { message: String, line: usize },
 }
+
+impl std::error::Error for Error {}
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
